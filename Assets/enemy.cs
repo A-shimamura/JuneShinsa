651384af7acd9fffb5dd45d8926d3240a3512bfb
A-shimamura <Nitopasu@gmail.com>
2024-06-01 12:17:57 +0900

    2倍

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -7,20 +7,25 @@ public class enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject _shoot;
-    [SerializeField] float _turn=0;
+    [SerializeField] float _turn1=0;
+    [SerializeField] float _turn2=180;
     Vector2 Vector2 = Vector2.zero;
     void Start()
     {
-        
+        Application.targetFrameRate = 120;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 = this.transform.position;
-        var g = Instantiate(_shoot,this.transform.position,Quaternion.Euler(0, 0, _turn));
-        _turn += 1.5f;
-        Vector2.x += 1*Time.deltaTime;
+
+        var g1 = Instantiate(_shoot,this.transform.position,Quaternion.Euler(0, 0, _turn1));
+        _turn1 += 5f;
+        var g2 = Instantiate(_shoot, this.transform.position, Quaternion.Euler(0, 0, _turn2));
+        _turn2 += 5f;
+
+        Vector2.x += 2*Time.deltaTime;
         this.transform.position = Vector2;
     }
 }
--- Assets/shotmanage.cs
+++ Assets/shotmanage.cs
@@ -6,6 +6,7 @@ public class shotmanage : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]float speed = 1;
+    float time;
     void Start()
     {
         
@@ -14,8 +15,10 @@ public class shotmanage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        time += Time.deltaTime;
         Vector3 velocity = gameObject.transform.rotation * new Vector3(speed, 0, 0);
         gameObject.transform.position += velocity * Time.deltaTime;
+        if (time >= 2) speed = 5f;
+        if (time >= 5) Destroy(this.gameObject);
     }
 }
